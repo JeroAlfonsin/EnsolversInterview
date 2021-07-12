081907e94d48813df78160e225e39a983fc809e5
JeroAlfonsin <55150040+JeroAlfonsin@users.noreply.github.com>
2021-07-12 08:25:05 -0300

    Add files via upload

---
+++ initFrontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd frontend_es
+npm install bootstrap
+npm install reacstrap
+npm install axios
+npm install --save @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+npm start
