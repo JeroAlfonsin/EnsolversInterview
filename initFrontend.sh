5351cdcd76007b91a4fb27508e20bb7c08e89a7e
JeroAlfonsin <55150040+JeroAlfonsin@users.noreply.github.com>
2021-07-12 08:25:53 -0300

    Delete initFrontend.sh

--- initFrontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd frontend_es
-npm install bootstrap
-npm install reacstrap
-npm install axios
-npm install --save @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
-npm start
